1eae37b5b5aa4aaddf1520d2b7fbb01ccc098457
rohit-0000 <rohit20040121@gmail.com>
2025-05-02 16:14:51 +0530

    adding delete chat option

--- chatAppFrontEnd/src/Components/signup.jsx
+++ chatAppFrontEnd/src/Components/signup.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import "../app.css";
 import { Link, useNavigate } from "react-router";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
