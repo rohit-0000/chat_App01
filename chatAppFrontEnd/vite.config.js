7ae02752154abc160345f27ad6ace1a1fe6a5097
rohit-0000 <rohit20040121@gmail.com>
2025-05-03 20:32:16 +0530

    adding Oauth2

--- chatAppFrontEnd/vite.config.js
+++ chatAppFrontEnd/vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite' 
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-  base: "/",
+  plugins: [react(), tailwindcss()],
+  base: '/',
   build: {
-    outDir: "dist"
+    outDir: 'dist',
   },
-  server: {
-    historyApiFallback: true
-  }
-})
+  define: {
+    global: {}, // Fix "global is not defined" error
+  },
+});
